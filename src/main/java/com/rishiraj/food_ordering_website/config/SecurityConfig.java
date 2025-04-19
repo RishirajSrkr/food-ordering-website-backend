6b9da065613849710eb3b3c971d224304e356701
RishirajSrkr <113599778+RishirajSrkr@users.noreply.github.com>
2025-04-19 14:03:38 +0530

    cors

--- src/main/java/com/rishiraj/food_ordering_website/config/SecurityConfig.java
+++ src/main/java/com/rishiraj/food_ordering_website/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174", "https://food-ordering-website-frontend-customers.vercel.app/", "https://food-ordering-website-frontend-admin.vercel.app/"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174", "https://food-ordering-website-frontend-customers.vercel.app", "https://food-ordering-website-frontend-admin.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")); // Allow these methods
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type")); // Allow headers
         configuration.setAllowCredentials(true); // Allow cookies or credentials
--- src/main/java/com/rishiraj/food_ordering_website/enums/FoodCategory.java
+++ src/main/java/com/rishiraj/food_ordering_website/enums/FoodCategory.java
@@ -1,12 +1,11 @@
 package com.rishiraj.food_ordering_website.enums;
 
 public enum FoodCategory {
-    BIRYANI,
-    SALAD,
-    ROLLS,
-    FRIED_RICE,
-    ICE_CREAM,
-    CAKE,
-    BURGER,
-    PIZZA
+    SEASONAL,
+    ORGANIC,
+    EXOTIC,
+    LOCAL,
+    CITRUS,
+    BERRIES,
+    STONE_FRUITS
 }
